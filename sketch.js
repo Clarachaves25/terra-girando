d48d7eb93957e2fa212cd2b10e64da5e93099126
Clarachaves25 <ana.chaves.borges@escola.pr.gov.br>
2025-07-29 11:48:45 -0300

    a

--- sketch.js
+++ sketch.js
@@ -4,10 +4,10 @@ function setup() {
 }
 
 function draw() {
-  background(205, 102, 94); //cor de fundo
+  background(210, 304, 98); //cor de fundo
   rotateY(millis()/1000); // rotação da terra
   texture(terra); //textura da terra, imagem.
-  sphere(80, 100); //esfera 3D
+  sphere(80, 500); //esfera 3D
   
   
 }
